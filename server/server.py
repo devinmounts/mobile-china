6e0e0491e2cef4cb8ff1f1352aaa0d98804ec69f
Devin Mounts <devinmounts@gmail.com>
2019-02-06 15:06:52 -0800

    connect to mysql db.

--- server/server.py
+++ server/server.py
@@ -1,11 +1,20 @@
 import pandas as pd
 from shapely.geometry import Point, shape
-
+import mysql.connector
 from flask import Flask
 from flask import render_template
 import json
 
 app = Flask(__name__)
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'unix_socket': '/Applications/MAMP/tmp/mysql/mysql.sock',
+  'database': 'mobile_china',
+  'raise_on_warnings': True,
+}
+link = mysql.connector.connect(**config)
 
 @app.route("/")
 def index():
@@ -13,6 +22,7 @@ def index():
 
 @app.route("/data")
 def get_data():
+    
     return "Get data works!"
 
 if __name__ == "__main__":
