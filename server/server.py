e6f5446d1bb58453189041af845cafd63253e54a
Devin Mounts <devinmounts@gmail.com>
2019-02-06 17:28:31 -0800

    sql statement efficiency

--- server/server.py
+++ server/server.py
@@ -18,12 +18,25 @@ link = mysql.connector.connect(**config)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return 'index'
+    # render_template("index.html")
 
 @app.route("/data")
 def get_data():
-    
-    return "Get data works!"
+    mycursor = link.cursor()
+    print(mycursor, 'before SQL')
+    mycursor.execute("SELECT * FROM gender_age_train LEFT JOIN events ON gender_age_train.device_id = events.device_id LEFT JOIN phone_brand_device_model ON gender_age_train.device_id = phone_brand_device_model.device_id LIMIT 10")
+    print('after sql')
+
+    SELECT * FROM gender_age_train INNER JOIN events ON gender_age_train.device_id = events.device_id INNER JOIN phone_brand_device_model ON gender_age_train.device_id = phone_brand_device_model.device_id LIMIT 10
+    # myresult = mycursor.fetchall()
+
+    # resultArray = []
+    # print('before loop')
+    # for entry in myresult:
+    #   resultArray.append(entry)
+
+    return 'data works'
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000, debug=True)
