c8f693832cdf97ffd95091145d9aa6697846d313
Devin Mounts <devinmounts@gmail.com>
2019-02-11 14:35:14 -0800

    update server.py

--- server/server.py
+++ server/server.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 from shapely.geometry import Point, shape
 import mysql.connector
@@ -5,8 +7,37 @@ from flask import Flask
 from flask import render_template
 import json
 
-app = Flask(__name__)
+data_path = '~/desktop/data/'
+n_samples = 30000
 
+#create age segments
+def get_age_segment(age):
+  if age <= 22:
+      return '22-'
+  elif age <= 26:
+      return '23-26'
+  elif age <= 28:
+      return '27-28'
+  elif age <= 32:
+      return '29-32'
+  elif age <= 38:
+      return '33-39'
+  else:
+      return '39+'
+
+# return point province name
+def get_location(longitude, latitude, provinces_json):
+
+  point = Point(longitude, latitude)
+  
+  for record in provinces_json['features']:
+      polygon = shape(record['geometry'])
+      if polygon.contains(point):
+          return record['properties']['name']
+  return 'other'
+
+with open('./../data/chinese_provinces_en.json') as data_file:
+  provinces_json = json.load(data_file)
 # config = {
 #   'user': 'root',
 #   'password': 'root',
@@ -18,18 +49,40 @@ app = Flask(__name__)
 
 # /Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot
 
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return 'index'
     # render_template("index.html")
 
+
 @app.route("/data")
 def get_data():
-  gen_age_train = pd.read_csv()
+    # import csv files
+    gen_age_train = pd.read_csv(data_path + 'gender_age_train.csv')
+    events = pd.read_csv(data_path + 'events.csv')
+    ph_br_dev_model = pd.read_csv(data_path + 'phone_brand_device_model.csv')
 
+    #create data frame
+    df = gen_age_train.merge(events, how='left', on='device_id')
+    df = df.merge(ph_br_dev_model, how='left', on='device_id')
+    
+    # get n_sample records
+    df = df[df['longitude'] != 0].sample(n=n_samples)
 
-    return 'data works'
+    top_10_brands_en = {'华为':'Huawei', '小米':'Xiaomi', '三星':'Samsung', 'vivo':'vivo', 'OPPO':'OPPO', 
+                      '魅族':'Meizu', '酷派':'Coolpad', '乐视':'LeEco', '联想':'Lenovo', 'HTC':'HTC'}
 
+    df['phone_brand_en'] = df['phone_brand'].apply(lambda phone_brand: top_10_brands_en[phone_brand] if (phone_brand in top_10_brands_en) else 'Other')
+    
+    df['age-segment'] = df['age'].apply(lambda age: get_age_segment(age))
+    
+    df['location'] = df.apply(lambda row: get_location(row['longitude'], row['latitude'], provinces_json), axis=1)
+
+    cols_to_keep = ["timestamp", 'longitude', 'latitude', 'phone_brand_en', 'gender', 'age_segment', 'location']
+    df_clean = df[cols_to_keep].dropna()
+    return df_clean.to_json(orient='records')
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000, debug=True)
